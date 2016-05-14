acdb4e5f0dd27a1946dace91e5ee29c236298a03
Jeff Lee <jeff@healthiq.com>
2016-05-14 11:26:58 -0700

    Updated to node v6 + Added Systemjs builder

---
+++ dist/system.config.js
@@ -0,0 +1,12 @@
+System.config({
+    defaultJSExtensions: true,
+    map: {
+        papaparse: "./node_modules/papaparse/papaparse.min.js",
+        zingchart: "./node_modules/zingchart/client/zingchart.min.js"
+    },
+    paths: {
+        'angular2/*': "./node_modules/angular2/*",
+        'rxjs/*': "./node_modules/rxjs/*",
+        'reflect-metadata/*': './node_modules/reflect-metadata/*'
+    }
+});
